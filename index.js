da065e6d4b768708dfb79284b28db58201683e4a
muhammadumar-codes <muhammadumar.codes@gmail.com>
2026-04-19 09:21:37 +0500

    bad commit message

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const first = 3;
+const first=3
 console.log(first);
+
+// This will cause a linting error - unused variable
+const unusedVariable = "this is not used";
+
+// This will cause a formatting error - wrong indentation
+  console.log("bad indentation");
